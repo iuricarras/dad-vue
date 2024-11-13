15e7fe9c9f1ecb1e4d2c628b66458d4cc0673e6a
Tendeiro2 <2222047@my.ipleiria.pt>
2024-11-13 19:54:40 +0000

    a

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,32 @@
 <script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { onMounted, useTemplateRef, provide } from 'vue'
+import { RouterView } from 'vue-router'
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+  <nav class="flex justify-between items-center bg-gray-800 p-3 text-white">
+    <!-- Memory Game como um link interativo para Home -->
+    <RouterLink
+      to="/home"
+      class="text-lg font-medium hover:text-blue-500 px-3 py-2 rounded-md transition-colors">
+      Memory Game
+    </RouterLink>
+    <!-- Links à direita para Register e Login -->
+    <div class="flex space-x-4">
+      <RouterLink
+        to="/register"
+        class="text-sm font-medium hover:text-blue-500 px-3 py-2 rounded-md transition-colors"
+        v-slot="{ isActive }">
+        <span :class="{ 'text-blue-500': isActive }">Register</span>
+      </RouterLink>
+      <RouterLink
+        to="/login"
+        class="text-sm font-medium hover:text-blue-500 px-3 py-2 rounded-md transition-colors"
+        v-slot="{ isActive }">
+        <span :class="{ 'text-blue-500': isActive }">Login</span>
+      </RouterLink>
     </div>
-  </header>
-
-  <RouterView />
+  </nav>
+  <RouterView></RouterView>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
-</style>
