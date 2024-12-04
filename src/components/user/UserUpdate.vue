6d76c79ba0f6cee3c162f4312d768880174ee4de
Zeca <2191762@my.ipleiria.pt>
2024-12-04 11:13:52 +0000

    UserUpdate-Fix

--- src/components/user/UserUpdate.vue
+++ src/components/user/UserUpdate.vue
@@ -16,6 +16,7 @@ import { Input } from '@/components/ui/input'
 import { Label } from '@/components/ui/label'
 
 const router = useRouter()
+const storeAuth = useAuthStore();
 const storeError = useErrorStore()
 
 const credentials = ref({
